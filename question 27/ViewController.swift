8ef4292a1026848709943dea3bbc0f3ae53b937e
Gevorg <gevorggsh@gmail.com>
2020-05-24 15:23:17 +0400

    colors done

--- question 27/ViewController.swift
+++ question 27/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outletButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func changePressed(_ sender: UIButton) {
+     let Color = getRandomColor()
+        sender.backgroundColor = Color
+    }
+    
+    
+    
+    private func getRandomColor () ->UIColor {
+        let randomRed = CGFloat.random(in: 0...1)
+        let randomGreen = CGFloat.random(in: 0...1)
+        let randomBlue = CGFloat.random(in: 0...1)
+        let color = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1)
+        return color
+    }
 }
 
